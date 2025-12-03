921909d3783a2b139eabeeefe036cd0c9b2d4e4f
Sebastian Bär <sebastian.baer@exasol.com>
2025-12-03 13:10:05 +0100

    Removed unnecessary class for factory.

--- tdbp/dialects/exasol/exasol_connection_factory.py
+++ tdbp/dialects/exasol/exasol_connection_factory.py
@@ -8,7 +8,3 @@ def connect() -> ExaConnection:
     return pyexasol.connect(dsn=f"localhost/{ExasolFingerprintProvider().fingerprint()}:8563", user="sys",
                             password="exasol",
                             autocommit=False)
-
-
-class ExasolConnectionFactory:
-    pass
