de1c788c4668b5114df564d86026ecbc85308f32
Sebastian Bär <sebastian.baer@exasol.com>
2025-12-04 10:36:03 +0100

    #3: Added nox configuration.

---
+++ noxconfig.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Iterable
+
+from exasol.toolbox.config import BaseConfig
+
+
+class Config(BaseConfig):
+    root: Path = Path(__file__).parent
+    doc: Path = Path(__file__).parent / "doc"
+    source: Path = Path("exasol/{{cookiecutter.package_name}}")
+    version_file: Path = (
+            Path(__file__).parent
+            / "exasol"
+            / "{{cookiecutter.package_name}}"
+            / "version.py"
+    )
+    plugins: Iterable[object] = ()
+
+PROJECT_CONFIG = Config()
