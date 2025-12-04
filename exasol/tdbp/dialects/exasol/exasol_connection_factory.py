01e7183777f3cb5d906d60c97b8ef52b2d25d9b4
Sebastian Bär <sebastian.baer@exasol.com>
2025-12-04 17:04:51 +0100

    #3: Refactored `ExasolFingerprintProvider` to use singleton pattern and implemented dynamic fingerprint extraction. Updated `ExasolConnectionFactory` to use the new instance.

--- exasol/tdbp/dialects/exasol/exasol_connection_factory.py
+++ exasol/tdbp/dialects/exasol/exasol_connection_factory.py
@@ -8,7 +8,7 @@ from exasol.tdbp.dialects.exasol.exasol_fingerprint_provider import (
 
 def connect() -> ExaConnection:
     return pyexasol.connect(
-        dsn=f"localhost/{ExasolFingerprintProvider().fingerprint()}:8563",
+        dsn=f"localhost/{ExasolFingerprintProvider.get_instance().fingerprint()}:8563",
         user="sys",
         password="exasol",
         autocommit=False,
